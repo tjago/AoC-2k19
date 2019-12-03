88c482bc03907b935f231101a0efdebfc618b0be
Tomek Jagodowski <tomek.jagodowski@.workday.com>
2019-12-03 00:03:31 +0000

    Day1 solution complete

--- src/eu/tjago/Day1.java
+++ src/eu/tjago/Day1.java
@@ -1,6 +1,8 @@
 package eu.tjago;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.OptionalInt;
 
 public class Day1 {
     public static void main(String[] args) {
@@ -9,6 +11,26 @@ public class Day1 {
 
     void Solution() {
         List<String> inputs = Common.getStringArraysOutOfFile("res/Day1Data1.txt");
-        inputs.stream().forEach(System.out::println);
+//        inputs.stream().forEach(System.out::println);
+
+        OptionalInt result = inputs.stream()
+                .map(Integer::parseInt)
+                .mapToInt(Day1::calculateMassByRecursion)
+                .reduce((left, right) -> left + right);
+
+        result.ifPresent(System.out::println);
+        }
+
+    static Integer calculateMass(Integer i) {
+        if (i < 6) return 0;
+        return i / 3 - 2;
+    }
+
+    static Integer calculateMassByRecursion(Integer mass) {
+        if (mass > 0) {
+            return calculateMass(mass) + calculateMassByRecursion(calculateMass(mass));
+        } else {
+            return 0;
+        }
     }
 }
