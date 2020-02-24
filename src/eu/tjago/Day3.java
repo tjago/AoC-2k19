71187165af04e85ae8cf642d65814a5e7e107d80
Tomek Jagodowski <tomek.jagodowski@.workday.com>
2020-02-24 23:39:19 +0000

    Day 3: half-solution, missing calculation finding intersection point

--- src/eu/tjago/Day3.java
+++ src/eu/tjago/Day3.java
@@ -6,18 +6,36 @@ import java.util.*;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 public class Day3 {
     public static void main(String[] args) {
         new Day3().solution();
     }
 
-    int [][] grid = new int[10_000][10_000];
-
     enum Direction { LEFT, RIGHT, UP, DOWN}
 
+    class LinePair {
+        Line one, two;
+
+        LinePair(Line a, Line b) {
+            this.one = a;
+            this.two = b;
+        }
+    }
+
+    class Line {
+        Point A, B;
+
+        public Line(Point a, Point b) {
+            A = a;
+            B = b;
+        }
+
+        boolean intersects(Line other) {
+            return Line2D.linesIntersect(this.A.x, this.A.y, this.B.x, this.B.y,
+                    other.A.x, other.A.y, other.B.x, other.B.y);
+        }
+    }
     class Step {
         Direction direction;
         int stepsDistance;
@@ -43,41 +61,6 @@ public class Day3 {
             return this.direction + " " + stepsDistance;
         }
     }
-    class GridCursor {
-        int cursorX;
-        int cursorY;
-
-        GridCursor() {
-            this.cursorX = 0;
-            this.cursorY = 0;
-        }
-        GridCursor(int startX, int startY) {
-            this.cursorX = startX;
-            this.cursorY = startY;
-        }
-
-        int getCursorX() {
-            return this.cursorX;
-        }
-
-        int getCursorY() {
-            return this.cursorY;
-        }
-
-        int incCursorX() {
-            return this.cursorX++;
-        }
-        int incCursorY() {
-            return this.cursorY++;
-        }
-        int decCursorX() {
-            return this.cursorX--;
-        }
-        int decCursorY() {
-            return this.cursorY--;
-        }
-
-    }
 
     private void solution() {
 
@@ -89,61 +72,70 @@ public class Day3 {
                 Arrays.asList(Common.getStringArraysOutOfFile("res/Day3Data1.txt")
                         .get(1).split(","));
 
-        LinkedList<Point> path1Points = populateGrid(wire1PathSteps);
-        LinkedList<Point> path2Points = populateGrid(wire2PathSteps);
+        List<Line> path1Lines = createLines(wire1PathSteps);
+        List<Line> path2Lines = createLines(wire2PathSteps);
 
+        List<LinePair> intersectingLines = new ArrayList<>();
 
-        IntStream.range(0, path1Points.size() - 1).forEach(
-                value -> IntStream.range(0, path2Points.size() - 1).filter(
-                        value1 -> linesIntersect(path1Points.get(value), path1Points.get(value +1),
-                                path2Points.get(value1), path2Points.get(value1 +1))
-                ).peek(System.out::println)
+        IntStream.range(0, path1Lines.size() - 1).forEach(
+                value -> IntStream.range(0, path2Lines.size() - 1).forEach(
+                        value1 -> {
+                            if (path1Lines.get(value).intersects(path2Lines.get(value1))) {
+                                intersectingLines.add(new LinePair(path1Lines.get(value), path2Lines.get(value1)));
+                            }
+                        }
+                )
         );
+        System.out.println("Shortest distance for intersecting wires: ");
+    }
 
+    private OptionalInt shortestManhattanDistance(List<Point> intersectionPoints) {
 
-//        long wiresPathCount = IntStream.rangeClosed(0, 10_000).mapToLong(
-//                x -> IntStream.rangeClosed(0, 10_000).filter(
-//                        y -> grid[x][y] == 1
-//                ).count()
-//        ).sum();
+       return intersectionPoints
+               .stream()
+               .mapToInt(p -> Math.abs(p.x) + Math.abs(p.y))
+               .min();
 
-//        System.out.println(wiresPathCount);
     }
 
     boolean linesIntersect(Point a, Point b, Point c, Point d) {
+
+
         return Line2D.linesIntersect(a.x, a.y, b.x, b.y, c.x, c.y, d.x, d.y);
     }
 
-    private LinkedList<Point> populateGrid(List<String> wirePathSteps) {
+    private List<Line> createLines(List<String> wirePathSteps) {
 
-        List<Step> pathSteps = wirePathSteps.stream()
-                .map(Step::new).collect(Collectors.toList());
+        List<Step> pathSteps = wirePathSteps
+                .stream()
+                .map(Step::new)
+                .collect(Collectors.toList());
 
-        GridCursor gc = new GridCursor(5_000, 5_000);
-
-        LinkedList<Point> path1Points = new LinkedList<>();
-        path1Points.add(new Point(0,0));
-
-        int cursorX = 0, cursorY = 0;
+        LinkedList<Point> points = new LinkedList<>();
+        points.add(new Point(0,0));
 
         pathSteps.forEach(step -> {
-            Point prevPoint = path1Points.getLast();
+            Point prevPoint = points.getLast();
                     switch (step.direction) {
                         case UP:
-                            path1Points.addLast(new Point(prevPoint.x, prevPoint.y + step.stepsDistance));
+                            points.addLast(new Point(prevPoint.x, prevPoint.y + step.stepsDistance));
                             break;
                         case DOWN:
-                            path1Points.addLast(new Point(prevPoint.x, prevPoint.y - step.stepsDistance));
+                            points.addLast(new Point(prevPoint.x, prevPoint.y - step.stepsDistance));
                             break;
                         case LEFT:
-                            path1Points.addLast(new Point(prevPoint.x - step.stepsDistance, prevPoint.y));
+                            points.addLast(new Point(prevPoint.x - step.stepsDistance, prevPoint.y));
                             break;
                         case RIGHT:
-                            path1Points.addLast(new Point(prevPoint.x + step.stepsDistance, prevPoint.y));
+                            points.addLast(new Point(prevPoint.x + step.stepsDistance, prevPoint.y));
                             break;
                     }
                 });
 
-        return path1Points;
+
+        return IntStream
+                .range(0, pathSteps.size() -1)
+                .mapToObj(val -> new Line(points.get(0), points.get(+1)))
+                .collect(Collectors.toList());
     }
 }
