e0677dc618ae28bc3cdf2d7619e62a987023eb6a
Tomek Jagodowski <tomek.jagodowski@.workday.com>
2020-03-14 13:01:48 +0000

    Day 6 solution part 2 complete

--- src/eu/tjago/Day6.java
+++ src/eu/tjago/Day6.java
@@ -34,6 +34,35 @@ public class Day6 {
         });
 
         System.out.println("All orbits of objects = " + counter);
+
+        //Part 2
+
+        AtomicInteger minDistance = new AtomicInteger(Integer.MAX_VALUE);
+        orbits.forEach((s, strings) -> {
+            Distance distToSan = distanceFromObject(s, "SAN" , orbits);
+            Distance distToYou = distanceFromObject(s, "YOU" , orbits);
+            if (distToSan.isFound() && distToYou.isFound()) {
+                int temporaryDistance = distToSan.getDist() + distToYou.getDist() -2; //-2 because we don't count YOU and SAN as orbits
+                if (temporaryDistance < minDistance.get()) {
+                    minDistance.set(temporaryDistance);
+                }
+            }
+        });
+        System.out.println("Minimum distance between Santa and you is: " + minDistance);
+    }
+
+    Distance distanceFromObject(String currentObject, String target, Map<String, List<String>> orbits) {
+        if (orbits.get(currentObject) == null) {
+            return currentObject.equals(target) ? new Distance(true, 0) : new Distance(false, 0);
+        } else {
+            for (String object : orbits.get(currentObject)) {
+                Distance search = distanceFromObject(object, target, orbits);
+                if(search.isFound()) {
+                    return new Distance(true, search.getDist() + 1);
+                }
+            }
+            return new Distance(false, 0);
+        }
     }
 
     int countOrbitsOf(String currentObject, Map<String, List<String>> orbits) {
@@ -45,6 +74,27 @@ public class Day6 {
                 count += 1 + countOrbitsOf(object, orbits);
             }
             return count;
+        }
+    }
+    class Distance {
+        private boolean found = false;
+        private int dist = 0;
+
+        public Distance(boolean found, int dist) {
+            this.found = found;
+            this.dist = dist;
+        }
+
+        private void incrementBy(int val) {
+            dist+= val;
+        }
+
+        public boolean isFound() {
+            return found;
+        }
+
+        public int getDist() {
+            return dist;
         }
     }
 
